7ea1cfd0487da299d176caa0dd5cef6ea8db5a16
Bane <branislavbogosavac99@gmail.com>
2024-09-21 12:17:55 -0600

    week01

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string FName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string LName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LName}, {FName} {LName}");
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade percentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        string grade;
+
+        if (percentage >= 90) {
+            grade = "A";
+
+        }
+        else if(percentage >= 80){
+            grade = "B";
+
+        }
+        else if(percentage >= 70){
+            grade = "C";
+        }
+        else if (percentage >= 60){
+            grade = "D";
+        }
+        else{
+            grade = "F";
+        }
+
+        if (percentage % 10 >= 7 && grade != "A" && grade != "F") {
+            grade += "+";
+        }
+        else if (percentage % 10 < 3 && grade != "F") {
+            grade += "-";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
     }
 }
