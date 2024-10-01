86fac9db54afd4f1b7f7a7258eb8e9caba4d30e7
Branislav Bogosavac <bbogosavac1999@gmail.com>
2024-10-01 08:43:18 -0600

    learnin02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Developer";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+        // job1.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Branislav Bogosavac";
+        resume1._jobs.Add(job1);
+        resume1.Display();
+
+
     }
 }
