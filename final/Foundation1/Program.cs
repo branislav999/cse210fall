e1db44af3e5304fa8dddf55b32b29a5e51ae6a90
Branislav Bogosavac <97567320+branislav999@users.noreply.github.com>
2024-12-07 21:03:25 -0700

    done w12

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video v1 = new Video("Sta ima buraz","Ljubi Chika", 47);
+        Comment c1 = new Comment("Josh", "great video");
+        Comment c2 = new Comment("Mary", "I liked the point of the video");
+        Comment c3 = new Comment("Alex", "good work");
+        v1.AddComment(c1);
+        v1.AddComment(c2);
+        v1.AddComment(c3);
+        v1.Display();
+        
+        Video v2 = new Video("How to cook pasta","Chef", 420);
+        Comment c4 = new Comment("Lily", "Very helpful");
+        Comment c5 = new Comment("Phil", "You cooked the pasta perfectly");
+        Comment c6 = new Comment("George", "My kids didn't like your way of making pasta");
+        v2.AddComment(c4);
+        v2.AddComment(c5);
+        v2.AddComment(c6);
+        v2.Display();
+        
+                
+
     }
 }
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,58 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+
+public class Video
+{
+    public string _title;
+    public string _author;
+    public int _seconds;
+
+    List<Comment> _comments = new List<Comment>{};
+
+    public Video(string title, string author, int seconds)
+    {
+        _title = title;
+        _author = author;
+        _seconds = seconds;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"\nTitle: {_title}, Author: {_author}, Length: {_seconds} seconds.");
+        Console.WriteLine($"Number of Comments on this video is: {CommentCount()}");
+        foreach (Comment com in _comments)
+        {
+            Console.WriteLine($"User {com._name} commented: {com._text} ");
+        }
+    }
+
+
+    public int CommentCount()
+    {
+        int sum = 0;
+        foreach (Comment comment in _comments)
+        {
+            sum++;
+        }
+        return sum;
+    }
+}
+
+public class Comment
+{
+    public string _name;
+    public string _text;
+
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+
+}
