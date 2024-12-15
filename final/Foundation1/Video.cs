421d435d14b9c487a5c2b095c507ea457f88f05d
Branislav Bogosavac <97567320+branislav999@users.noreply.github.com>
2024-12-15 02:05:59 -0700

    done

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -44,15 +44,3 @@ public class Video
     }
 }
 
-public class Comment
-{
-    public string _name;
-    public string _text;
-
-    public Comment(string name, string text)
-    {
-        _name = name;
-        _text = text;
-    }
-
-}
